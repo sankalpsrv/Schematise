7e53fa4383e0d22c86e8338ca49bc19862863148
Sankalp Srivastava <sankalpsrv@protonmail.com>
2024-03-09 19:56:02 +0530

    Added Disclaimer

--- Schematise.py
+++ Schematise.py
@@ -6,13 +6,7 @@ import IK_templates
 
 
 
-filename = "fullsections.csv"
 
-if 'openai_key' not in st.session_state:
-    st.session_state['openai_key'] = ''
-
-if 'ik_api' not in st.session_state:
-    st.session_state['ik_api'] = ''
 
 def set_env(key = '', request = ''):
     global ik_api, openai_key
@@ -38,15 +32,6 @@ def get_XML(df2, llm_selected, format_chosen):
     XML_responses = combinedProcess.responseGetter(openai_key, df2, llm_selected, format_chosen)
     return XML_responses
 
-ik_api, openai_key = set_env()
-
-st.session_state['openai_key'] = openai_key
-st.session_state['ik_api'] = ik_api
-
-st.image('Schematise-logo-light.png')
-
-condition_for_csv = st.radio("Do you want to upload a CSV file or use your IndianKanoon API key?", ["Upload", "IndianKanoon"])
-
 def dataframe_view(filename):
 
     st.write(f"First five rows of uploaded file")
@@ -57,100 +42,135 @@ def dataframe_view(filename):
 
     st.table(dftest2)
 
-if condition_for_csv == "Upload":
-    st.write(
-            "Upload a csv file in the same format as 'fullsections.csv' available at https://raw.githubusercontent.com/sankalpsrv/Schematise/dev/fullsections.csv and shown below")
+def main():
+    
+    filename = "fullsections.csv"
 
-    dataframe_view("fullsections.csv")
+    if 'openai_key' not in st.session_state:
+        st.session_state['openai_key'] = ''
 
-    uploaded_file = st.file_uploader("Choose a file")
+    if 'ik_api' not in st.session_state:
+        st.session_state['ik_api'] = ''
 
-    if uploaded_file is not None:
-        filename = uploaded_file.name
+    
+    ik_api, openai_key = set_env()
 
-else:
-    if ik_api == '':
-        ik_api=st.text_input("Please enter your IndianKanoon API Key")
-        set_env(ik_api, "ikanoon")
+    st.session_state['openai_key'] = openai_key
+    st.session_state['ik_api'] = ik_api
+
+    st.image('Schematise-logo-light.png')
+
+    condition_for_csv = st.radio("Do you want to upload a CSV file or use your IndianKanoon API key?", ["Upload", "IndianKanoon"])
+
+
+
+    if condition_for_csv == "Upload":
+        st.write(
+                "Upload a csv file in the same format as 'fullsections.csv' available at https://raw.githubusercontent.com/sankalpsrv/Schematise/dev/fullsections.csv and shown below")
+
+        dataframe_view("fullsections.csv")
+
+        uploaded_file = st.file_uploader("Choose a file")
+
+        if uploaded_file is not None:
+            filename = uploaded_file.name
 
     else:
-        print(f"IndianKanoon API key is {ik_api}")
-        pass
-    docnumber=st.text_input("Specify a document number to get from IndianKanoon")
+        if ik_api == '':
+            ik_api=st.text_input("Please enter your IndianKanoon API Key")
+            set_env(ik_api, "ikanoon")
 
-    IK_templates.extract_text(docnumber, ik_api)
+        else:
+            print(f"IndianKanoon API key is {ik_api}")
+            pass
+        docnumber=st.text_input("Specify a document number to get from IndianKanoon")
 
-    filename = "sections.csv"
+        IK_templates.extract_text(docnumber, ik_api)
 
-    dataframe_view("sections.csv")
+        filename = "sections.csv"
 
-format_chosen=st.radio(
-        "Choose LegalDocML/AkomaNtoso or LegalRuleML 👉",
-        key="format_chosen",
-        options=["None", "legaldocml", "legalruleml"],
-    )
+        dataframe_view("sections.csv")
 
-
-start_value = st.text_input('Starting range', '71')
-end_value = st.text_input('Ending range', '73')
-df2 = load_data(filename, start_value, end_value)
-
-view_df = st.checkbox("Show Dataframe")
-
-if view_df:
-    st.table(df2)
-
-llm_selected = st.radio(
-        "Choose OpenAI or Llama2 👉",
-        key="llm_selected",
-        options=["None", "OpenAI"], #Option for Llama2-7b-chat removed
-    )
+    format_chosen=st.radio(
+            "Choose LegalDocML/AkomaNtoso or LegalRuleML 👉",
+            key="format_chosen",
+            options=["None", "legaldocml", "legalruleml"],
+        )
 
 
-if llm_selected == 'OpenAI' and openai_key == '':
-    print ("Session state variable for OpenAI is", st.session_state['openai_key'])
-    openai_key_input = st.text_input("OpenAI API Key")
-    set_env(openai_key_input, "openai")
+    start_value = st.text_input('Starting range', '71')
+    end_value = st.text_input('Ending range', '73')
+    df2 = load_data(filename, start_value, end_value)
 
-try:
-    XML_responses = get_XML(df2, llm_selected, format_chosen)
-    st.download_button(label = "Download XML generated", 
-                        data = str(XML_responses),
-                        file_name = f'{format_chosen}_{start_value}_{end_value}.txt')
+    view_df = st.checkbox("Show Dataframe")
 
-except ValueError:
-    st.write("Error: Please enter a valid OpenAI key")
+    if view_df:
+        st.table(df2)
 
-except UnboundLocalError:
-    st.write("Error: Please follow all earlier steps for selecting format, number of sections, and llm")
+    llm_selected = st.radio(
+            "Choose OpenAI or Llama2 👉",
+            key="llm_selected",
+            options=["None", "OpenAI"], #Option for Llama2-7b-chat removed
+        )
 
 
-try:
-    st.write(XML_responses)
+    if llm_selected == 'OpenAI' and openai_key == '':
+        print ("Session state variable for OpenAI is", st.session_state['openai_key'])
+        openai_key_input = st.text_input("OpenAI API Key")
+        set_env(openai_key_input, "openai")
 
-except NameError:
-    st.write("Error: Please complete the earlier steps for computing XML_responses")
+    try:
+        XML_responses = get_XML(df2, llm_selected, format_chosen)
+        st.download_button(label = "Download XML generated", 
+                            data = str(XML_responses),
+                            file_name = f'{format_chosen}_{start_value}_{end_value}.txt')
 
-try:
-    st.session_state['XML_resp'] = XML_responses
+    except ValueError:
+        st.write("Error: Please enter a valid OpenAI key")
 
-except NameError:
-    st.write("Error: Please complete the earlier steps for storing XML_responses")
-
-try:
-    st.session_state['fchosen'] = format_chosen
-
-except NameError:
-    st.write("Error: Please complete the earlier steps for selecting format: LegalDocML or LegalRuleML")
-
-try:
-    st.session_state['llmc'] = llm_selected
-
-except NameError:
-    st.write("Error: Please complete the earlier steps for selecting LLM")
+    except UnboundLocalError:
+        st.write("Error: Please follow all earlier steps for selecting format, number of sections, and llm")
 
 
+    try:
+        st.write(XML_responses)
 
+    except NameError:
+        st.write("Error: Please complete the earlier steps for computing XML_responses")
+
+    try:
+        st.session_state['XML_resp'] = XML_responses
+
+    except NameError:
+        st.write("Error: Please complete the earlier steps for storing XML_responses")
+
+    try:
+        st.session_state['fchosen'] = format_chosen
+
+    except NameError:
+        st.write("Error: Please complete the earlier steps for selecting format: LegalDocML or LegalRuleML")
+
+    try:
+        st.session_state['llmc'] = llm_selected
+
+    except NameError:
+        st.write("Error: Please complete the earlier steps for selecting LLM")
+
+
+if __name__ == "__main__":
+
+    buttonpress = False
+    st.error("Disclaimer: This is an AI application that will generate XML output for a law inputted by you. It is not a replacement for legal advice.")
+
+    buttonpress= st.button("Yes, I understand. Please take me to the application")
+
+    if buttonpress is True:
+        st.error("You have accepted the disclaimer.")
+        main()
+        
+
+        
+   
 
 
 
