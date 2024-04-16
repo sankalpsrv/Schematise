67b8dfddb404dd4373c64800a6e5f68227b40617
Sankalp Srivastava <sankalpsrv@protonmail.com>
2024-04-16 18:27:12 +0530

    added optional statements checklist

--- app.py
+++ app.py
@@ -52,16 +52,19 @@ def selection():
 def obligation():
     root = parse_xml_file("XML+validation/Modified-XML.xml")
 
+    # Get the value of paragraph_selections from previous page which was saved in a cookie
     paragraph_selections = session["paragraph_selections"]
 
     #print(paragraph_selections)
 
     ps_keys = []
 
+    #Store all prescriptive statement keys for the paragraphs selected
     for paragraph_number in paragraph_selections:
         if paragraph_selections[paragraph_number] == True:
             ps_keys.append(paragraph_number)
 
+    # Calling the extract_atoms_details function from xml_ops.py for getting the conditions which are applicable for each of the paragraphs selected
     atoms_data, atoms_keys = extract_atoms_details(root, ps_keys)
 
     #print ("ps_keys is", ps_keys)
@@ -70,13 +73,13 @@ def obligation():
 
     ## Hereunder, combining both the dictionaries
 
-
-
     for key in atoms_data:
         atoms_data[key].extend(atoms_keys[key])
 
 
     print ("atoms_data combined is", atoms_data)
+
+    # Variable named "additional_elements" gets all the additional_elements for the atoms_data whereas, <br>
 
     tree = parse_xml_file_lxml("XML+validation/Modified-XML.xml")
 
@@ -84,61 +87,90 @@ def obligation():
 
     print(additional_elements, "are the additional_alements generated")
 
-    additional_elements_applicable = {}
 
-
+    # iterating through each of the additional elements
 
     for key_of_additional_elements, value_of_additional_elements in additional_elements.items():
+        # splitting each additional element key to get the paragraph number
         key_trimmed = ''
         key_trimmed += key_of_additional_elements.split('_')[0]
+        # checking if the paragraph number is in the paragraphs selected by the user
         if key_trimmed in ps_keys:
+            # if present, appending to the list of compliances for that paragraph
             atoms_data[key_trimmed].append(value_of_additional_elements)
-
-    print ("additional_elements_applicable", additional_elements_applicable)
-
-    for key, value in additional_elements_applicable.items():
-        if atoms_data[key] is not None:
-            print("value is", value)
-            atoms_data[key].append(value)
 
     print ("atoms_data with additional_elements", atoms_data)
 
-        ## conditions_selected is created hereunder by filling in values from total_condition_statements if they have been selected
+
+
     conditions_applicable = {}
+    optional_conditions = {}
+    # iterating through the keys and values of the total applicable compliances stored paragraph-wise in atoms_data
     for key, values_list in atoms_data.items():
         for value in values_list:
             print ("value is", value)
+            # checks if the compliance is one that was extracted from the parent blocks of the atoms
             if value in total_condition_statements:
+                # conditions_applicable holds the atomic elements with the dictionary value being the statement for compliance
+                # which was extracted from the conditions.py file's variable named "total_condition_statements"
                 conditions_applicable[value] = total_condition_statements[value]
+            # checks if the compliance is one that was extracted from the call for check_for_additional_elements_lxml(tree,atoms_data)
             if value in extra_conditions:
-                conditions_applicable[value] = extra_conditions[value]
-                print("extra_condition added", extra_conditions[value])
+                # optional_conditions holds the optional elements extracted earlier with the dictionary value being
+                # the value extracted from conditions.py file's variable named "extra_conditions"
+                optional_conditions[value] = extra_conditions[value]
+                #print("extra_condition added", extra_conditions[value])
 
+    print("optional_conditions", optional_conditions)
 
+    print("applicable conditions", conditions_applicable)
     #print (conditions_selected)
 
+    # processes form data
     if request.method == "POST":
         conditions_selected = {}
 
+        optional_conditions_selected = {}
+
+        # gets values for both the conditions from parent blocks and optional conditions for each paragraph
         for key in conditions_applicable.keys():
             # request.form.get returns None if the key is not present, indicating an unchecked box
             conditions_selected[key] = bool(request.form.get(key))
 
-        print("Conditions selected are", conditions_selected)
-        print("Conditions applicable are", conditions_applicable)
+        for key in optional_conditions.keys():
+            # request.form.get returns None if the key is not present, indicating an unchecked box
+            optional_conditions_selected[key] = bool(request.form.get(key))
+
+        print("Conditions selected are", conditions_selected, optional_conditions_selected)
+        #print("Conditions applicable are", conditions_applicable, optional_conditions)
 
         conditions_met = []
-        # Iterate through each key-value pair in dict1
+
+        # Iterate through each key-value pair in atoms_data to check if any of the paragraphs' conditions are met by the checklist submitted
         for key, values in atoms_data.items():
-            # Check if all values for this key are True in dict2
-            if all(conditions_selected.get(value, False) for value in values):
-                # If all values are True, add the key to the satisfied keys list
-                conditions_met.append(key)
+            # Filter values to include only those that have corresponding keys in conditions_selected
+            filtered_values = [value for value in values if value in conditions_selected]
+            filtered_optional_values = [value for value in values if value in optional_conditions_selected]
+
+            print("Values being checked are", filtered_values)  # Updated to print filtered values
+            print ("Filtered_optional_values are", filtered_optional_values)
+
+            # Check if all filtered values are True in conditions_selected
+            if all(conditions_selected[value] for value in
+                   filtered_values):  # Here we directly access the value as the existence is guaranteed
+                # Check if any condition is True in optional_conditions_selected for the given values
+                if any(optional_conditions_selected.get(value, False) for value in
+                       filtered_optional_values):  # Using False as default if key doesn't exist
+                    # If any values are True, add the key to the satisfied keys list
+                    conditions_met.append(key)
+
+                elif len(filtered_optional_values) == 0:
+                    conditions_met.append(key)
 
         print ("Conditions met are", conditions_met)
 
-        return render_template("conditions.html", conditions_applicable=conditions_applicable, additional_elements_applicable=additional_elements_applicable, conditions_met=conditions_met, total_obligation_statements=total_obligation_statements)
+        return render_template("conditions.html", conditions_applicable=conditions_applicable, optional_conditions=optional_conditions, conditions_met=conditions_met, total_obligation_statements=total_obligation_statements)
 
-    return render_template("conditions.html", conditions_applicable = conditions_applicable, additional_elements_applicable=additional_elements_applicable)
+    return render_template("conditions.html", conditions_applicable = conditions_applicable, optional_conditions=optional_conditions)
 
 
